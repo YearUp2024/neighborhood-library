95ac67d1809a5afd2773a75422a94b356cda11cd
iftekar2 <iftekar.hossain123@gmail.com>
2024-10-08 14:40:58 -0400

    Adding comments

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,4 +1,3 @@
-
 package com.pluralsight;
 import java.util.Scanner;
 
@@ -6,6 +5,10 @@ public class Main{
     public static Book[] library;
     static Scanner scanner = new Scanner(System.in);
 
+    /**
+     * This is the Main method.
+     * @param args
+     */
     public static void main(String[] args){
         library = GetInitializedLibrary();
 
@@ -29,7 +32,11 @@ public class Main{
         }while(option != 'X');
     }
 
-    //This is going to return Available Books
+    /**
+     * GetAvailableBooks method is going to return Available Books to the users
+     * @param books
+     * @return All available Books
+     */
     public static Book[] GetAvailableBooks(Book[] books){
         Book[] availableBooks = new Book[books.length];
         int nextIndex = 0;
@@ -46,6 +53,12 @@ public class Main{
     }
 
     //This is going to return Checkout books
+
+    /**
+     * GetCheckedOutBooks method is going to return all the Books that were checked out
+     * @param books
+     * @return All checked out books
+     */
     public static Book[] GetCheckedOutBooks(Book[] books){
         Book[] checkedOut = new Book[books.length];
         int nextIndex = 0;
@@ -61,7 +74,10 @@ public class Main{
         return result;
     }
 
-    //This is going to show the user whether books are available or checked out.
+    /**
+     * DisplayBooks method is going to show all whether a book was checked out/to whom or if it is available.
+     * @param books
+     */
     public static void DisplayBooks(Book[] books){
         for (Book book : books) {
             if (book != null) {
@@ -71,7 +87,10 @@ public class Main{
         }
     }
 
-    //This will allow the user to choose from different options.
+    /**
+     * PromptChoices method will allow the use to choose from different choices.
+     * @return User Choice.
+     */
     public static char PromptChoices(){
         System.out.println("Welcome to the Library! Please select from the following options:");
         System.out.println("    Show [A]vailable Books");
@@ -105,7 +124,10 @@ public class Main{
         } while (true);
     }
 
-    //This will allow the user to check out book
+    /**
+     * CheckOutBook method allows the user to check out a book if it is available.
+     * @param library
+     */
     public static void CheckOutBook(Book[] library){
         System.out.println("Enter the ID of the Book you want to check out: ");
         int bookID = scanner.nextInt();
@@ -124,7 +146,10 @@ public class Main{
         System.out.println("Book ID is not found or the book is already checked out.");
     }
 
-    //This will allow the users to Return Book
+    /**
+     * ReturnBook method will allow the user to Return a book they have borrowed.
+     * @param library
+     */
     public static void ReturnBook(Book[] library){
         System.out.println("Enter the ID of the Book you want to return: ");
         int bookID = scanner.nextInt();
@@ -144,6 +169,10 @@ public class Main{
         }
     }
 
+    /**
+     * GetInitializedLibrary method show the user all the books they can borrow. This list will change if a book is not available.
+     * @return Library.
+     */
     public static Book[] GetInitializedLibrary() {
         Book[] library = new Book[20];
         library[0] = new Book(1, "Practical Tableau", "ISBN11332211");
@@ -170,114 +199,3 @@ public class Main{
         return library;
     }
 }
-
-//public class Main {
-//    public static Book[] library;
-//
-//    public static void main(String[] args){
-//        library = GetInitializedLibrary();
-//
-//        char option;
-//        do{
-//            option = PromptMainChoices();
-//            if(option == 'A'){
-//                Book[] availableBooks = GetAvailableBooks(library);
-//                DisplayBooks(availableBooks);
-//            }else if(option == 'C'){
-//                Book[] checkedOutBooks = GetCheckedOutBooks(library);
-//                DisplayBooks(checkedOutBooks);
-//            }
-//        }while(option != 'X');
-//    }
-//
-//    public static Book[] GetAvailableBooks(Book[] books){
-//        Book[] available = new Book[books.length];
-//
-//        int nextIndex = 0;
-//        for(Book book : books){
-//            if(!book.isCheckedOut()){
-//                available[nextIndex++] = book;
-//            }
-//        }
-//
-//        Book[] result = new Book[nextIndex];
-//        System.arraycopy(available, 0, result, 0, nextIndex);
-//        return result;
-//    }
-//
-//    public static Book[] GetCheckedOutBooks(Book[] books){
-//        Book[] checkedOut = new Book[books.length];
-//
-//        int nextIndex = 0;
-//        for(Book book : books){
-//            if(book.isCheckedOut()){
-//                checkedOut[nextIndex++] = book;
-//            }
-//        }
-//
-//        Book[] result = new Book[nextIndex];
-//        System.arraycopy(checkedOut, 0, result, 0, nextIndex);
-//        return result;
-//    }
-//
-//    public static void DisplayBooks(Book[] books){
-//        System.out.printf("%5s %55s %16s\n", "ID", "Title", "ISBN");
-//        for(Book book : books){
-//            System.out.printf("%5s %55s %20s %s\n", book.getId(), book.getTitle(), book.getISBN(), book.getCheckedOutTo());
-//        }
-//    }
-//
-//    public static char PromptMainChoices() {
-//        System.out.println("Welcome to the Library! Please select from the following choices:");
-//        System.out.println("    Show [A]vailable Books");
-//        System.out.println("    Show [C]hecked Out Books");
-//        System.out.println("    E[X]it the Library");
-//
-//        do {
-//            System.out.print("Command [A, C, X]: ");
-//            String command = Console.PromptForString();
-//
-//            if (command.equalsIgnoreCase("A")) {
-//                return 'A';
-//            }
-//            if (command.equalsIgnoreCase("C")) {
-//                return 'C';
-//            }
-//            if (command.equalsIgnoreCase("X")
-//                    || command.equalsIgnoreCase("EXIT")
-//                    || command.equalsIgnoreCase("Q")
-//                    || command.equalsIgnoreCase("QUIT")) {
-//                return 'X';
-//            }
-//        } while (true);
-//    }
-//
-//    public static Book[] GetInitializedLibrary() {
-//        Book[] library = new Book[20];
-//        library[0] = new Book(1, "Practical Tableau", "ISBN11332211");
-//        library[1] = new Book(2, "Pro Git", "ISBN1133229918");
-//        library[2] = new Book(3, "Genetic Programming", "ISBN2299220202");
-//        library[3] = new Book(4, "Clean Code", "ISBN9780136083238");
-//        library[4] = new Book(5, "Design Patterns", "ISBN9780201633610");
-//        library[5] = new Book(6, "Effective Java", "ISBN9780134686097");
-//        library[6] = new Book(7, "Java Concurrency in Practice", "ISBN9780321349606");
-//        library[7] = new Book(8, "The Pragmatic Programmer", "ISBN9780135957059");
-//        library[8] = new Book(9, "Introduction to the Theory of Computation", "ISBN9781133187790");
-//        library[9] = new Book(10, "Artificial Intelligence: A Modern Approach", "ISBN9780134610993");
-//        library[10] = new Book(11, "The Art of Computer Programming", "ISBN9780201896831");
-//        library[11] = new Book(12, "Python Crash Course", "ISBN9781593279288");
-//        library[12] = new Book(13, "The Clean Coder", "ISBN9780136083238");
-//        library[13] = new Book(14, "Code Complete", "ISBN9780735619678");
-//        library[14] = new Book(15, "Refactoring: Improving the Design of Existing Code", "ISBN9780134757599");
-//        library[15] = new Book(16, "Head First Design Patterns", "ISBN9780596007126");
-//        library[16] = new Book(17, "You Don't Know JS", "ISBN9781491950357");
-//        library[17] = new Book(18, "The Mythical Man-Month", "ISBN9780201835953");
-//        library[18] = new Book(19, "Learning JavaScript Data Structures and Algorithms", "ISBN9781785880332");
-//        library[19] = new Book(20, "The Elements of Programming Interviews", "ISBN9781512218237");
-//
-//        library[3].checkedOut("Iftekar Hossain");
-//        library[7].checkedOut("Very Long Named John");
-//
-//        return library;
-//    }
-//}
